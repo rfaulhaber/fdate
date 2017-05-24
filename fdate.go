fe34b5a070cdf6d327cfd6c9a571dc289489f63d
Ryan Faulhaber <faulhaber.14@osu.edu>
2017-05-24 18:08:34 -0400

    fdate struct reworked, more constants added

--- fdate.go
+++ fdate.go
@@ -1,16 +1,17 @@
 package fdate
 
+import "time"
+
 type FDate struct {
-	Month   string
-	Weekday string
-	DayName string // TODO: import these
-	Year    RomanNumeral
+	Month int
+	Day   int
+	Year  int
 }
 
 type Month int
 
 const (
-	Vendémiaire = 1 + iota
+	Vendémiaire Month = 1 + iota
 	Brumaire
 	Frimaire
 	Nivôse
@@ -24,7 +25,7 @@ const (
 	Fructidor
 )
 
-var months = [...]string {
+var months = [...]string{
 	"Vendémiaire",
 	"Brumaire",
 	"Frimaire",
@@ -39,10 +40,14 @@ var months = [...]string {
 	"Fructidor",
 }
 
+func (month Month) String() string {
+	return months[month-1]
+}
+
 type Weekday int
 
 const (
-	primidi = iota
+	primidi  Weekday = iota
 	duodi
 	tridi
 	quartidi
@@ -54,7 +59,7 @@ const (
 	décadi
 )
 
-var days = [...]string {
+var days = [...]string{
 	"primidi",
 	"duodi",
 	"tridi",
@@ -65,4 +70,47 @@ var days = [...]string {
 	"octidi",
 	"nonidi",
 	"décadi",
+}
+
+func (day Weekday) String() string {
+	return days[day]
+}
+
+type CompDay int
+
+const (
+	vertu       CompDay = iota
+	génie
+	travail
+	lOpinion
+	récompenses
+	révolution
+)
+
+var compDays = [...]string{
+	"La Fête de la Vertu",
+	"La Fête du Génie",
+	"La Fête du Travail",
+	"La Fête de l'Opinion",
+	"La Fête des Récompenses",
+	"La Fête de la Révolution",
+}
+
+func (day CompDay) String() string {
+	return compDays[day]
+}
+
+func Today() *FDate {
+
+}
+
+func NewFDate(day int, month int, year int) *FDate {
+}
+
+func NewDateFromTime(time time.Time) *FDate {
+
+}
+
+func (date *FDate) String() string {
+
 }
